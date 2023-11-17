020e83514bcbf3baf180aafbbcf762d0eb16db23
Samuel-Martineau <samuel@smartineau.me>
2023-11-17 11:38:42 -0500

    ajout de build-firefox-extension

--- default.nix
+++ default.nix
@@ -5,16 +5,14 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-
-{ pkgs ? import <nixpkgs> { } }:
-
-{
+{pkgs ? import <nixpkgs> {}}: {
   # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
+  lib = import ./lib {inherit pkgs;}; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  example-package = pkgs.callPackage ./pkgs/example-package {};
+
+  buildFirefoxExtension = pkgs.callPackage ./pkgs/build-firefox-extension {};
+  firefoxExtensionHooks = pkgs.callPackage ./pkgs/build-firefox-extension/hooks {};
 }
