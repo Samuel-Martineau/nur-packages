793f64ff9f01499a48f8111071a103e3aa2e20e4
Samuel-Martineau <samuel@smartineau.me>
2023-11-20 15:14:51 -0500

    mise à jour de pastebinit

--- pkgs/pastebinit/default.nix
+++ pkgs/pastebinit/default.nix
@@ -1,47 +1,74 @@
+# Copyright (c) 2003-2023 Eelco Dolstra and the Nixpkgs/NixOS contributors
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 { lib
 , stdenv
 , fetchurl
 , fetchpatch
 , python3
+, libxslt
+, docbook-xsl-ns
+, installShellFiles
+, wget
+, cacert
 }:
-stdenv.mkDerivation rec {
-  version = "1.5";
+stdenv.mkDerivation
+rec {
+  version = "1.6.2-1";
   pname = "pastebinit";
 
   src = fetchurl {
-    url = "https://launchpad.net/pastebinit/trunk/${version}/+download/${pname}-${version}.tar.bz2";
-    sha256 = "0mw48fgm9lyh9d3pw997fccmglzsjccf2y347gxjas74wx6aira2";
+    url = "https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/${pname}/${version}/${pname}_${lib.removeSuffix "-1" version}.orig.tar.gz";
+    sha256 = "b7f63207bfb66eca0e77052283815346d3ad9de2b30aa9f68b74521f0b54940a";
   };
+
+  patches = [
+    ./use-drv-etc.patch
+  ];
+
+  nativeBuildInputs = [
+    libxslt
+    installShellFiles
+  ];
 
   buildInputs = [
     (python3.withPackages (p: [ p.distro ]))
   ];
 
-  patchFlags = [ "-p0" ];
-
-  patches = [
-    # Required to allow pastebinit 1.5 to run on Python 3.8
-    ./use-distro-module.patch
-    # Required to remove the deprecation warning of FancyURLopener
-    ./use-urllib-request.patch
-    # Required because pastebin.com now redirects http requests to https
-    (fetchpatch {
-      name = "pastebin-com-https.patch";
-      url = "https://bazaar.launchpad.net/~arnouten/pastebinit/pastebin-com-https/diff/264?context=3";
-      sha256 = "0hxhhfcai0mll8qfyhdl3slmbf34ynb759b648x63274m9nd2kji";
-    })
-  ];
+  buildPhase = ''
+    xsltproc --nonet ${docbook-xsl-ns}/share/xml/docbook-xsl-ns/manpages/docbook.xsl pastebinit.xml
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
     mkdir -p $out/etc
     cp -a pastebinit $out/bin
     cp -a pastebin.d $out/etc
-    substituteInPlace $out/bin/pastebinit --replace "'/etc/pastebin.d" "'$out/etc/pastebin.d"
+    substituteInPlace $out/bin/pastebinit --subst-var-by "etc" "$out/etc"
+    installManPage pastebinit.1
   '';
 
   meta = with lib; {
-    homepage = "https://launchpad.net/pastebinit";
+    homepage = "https://launchpad.net/ubuntu/+source/pastebinit";
     description = "A software that lets you send anything you want directly to a pastebin from the command line";
     maintainers = with maintainers; [ raboof ];
     license = licenses.gpl2;
